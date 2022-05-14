0da5668288936ea5aa48b0e0350c3d6c4bf5b93d
Ahmed_mohamed <102621536+ahmedMhamed7@users.noreply.github.com>
2022-05-14 18:40:15 +0200

    added login function

--- CS112-203-2nd – SectionNumber -YourID-YourID-YourID-A4- LoginApp.cpp
+++ CS112-203-2nd – SectionNumber -YourID-YourID-YourID-A4- LoginApp.cpp
@@ -1,65 +1,216 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
 #include <algorithm>
 #include <cctype>
-#include <string>
 #include <conio.h>
-#include <unordered_map>
 #include <list>
 #include <regex>
-#include <fstream>
-
+#include <unordered_map>
 
 using namespace std;
 
-//Initializing variables
+struct user_profile{
+    string user_name;
+    string user_email;
+    string user_password;
+    string user_mobile;
+};
+
+map <string, user_profile> users;
+
+fstream users_file;
+
 int string_length, key = 3;
+string username, email, mobile, password;
+string user_choice;
 char matrix[100][100], encrypted[100];
 
-fstream myFile;
-string userEmail;
-unordered_map<string, list<string>> userProfile; // store email as key and list of details as value
-list<string> userDetails {}; // store list of details {mobile , name , password}
+//functions declaration
+void Register();
+void load_existing_users(map<string, user_profile> &my_map);
+bool is_username_existed(map <string, user_profile> &my_map, string username);
+bool is_email_existed(map <string, user_profile> &my_map, string email);
+bool is_password_existed(map <string, user_profile> &my_map);
+string verify_password();
+void initializing_matrix();
+void preparing_matrix_encryption(string text);
+void encryption();
+string encryptedPassword();
+bool has_any_digits(const string& password);
+bool check_uppercase(const string& password);
+bool check_lowercase(const string& password);
+bool contains_symbols(const string& password);
+bool contains_spaces(const string& password);
+void verifyName();
+void verifyMobile();
+void verifyEmail();
+void Login();
+//void LoginToChange();
+void change_password();
 
-// declaring functions
+int main(){
+    users_file.open("users_file.txt", ios:: app);
+    users_file.close();
+        while(true){
+            cout << "Welcome to login app! What do you want to do ?\n";
+            cout << "1-Register\n";
+            cout << "2-Login\n";
+            cout << "3-Change Password\n";
+            cout << "4-Exit\n";
+            cout << "Choice :";
+            load_existing_users(users);
+            cin >> user_choice;
+            if (user_choice == "1"){
+                Register();
+            }
+            else if (user_choice  == "2"){
+                Login();
+            }
+            else if (user_choice == "3"){
+                change_password();
+            }
+            else if (user_choice == "4"){
+                cout << "Thank you for using our app\n";
+                break;
+            }
+            else{
+                cout << "Please enter a valid choice \n";
+            }
+
+        }
+}
+
+
+
+
+void Register(){
+    /*myFile.open("users_file.txt", ios::in);
+    while (!myFile.eof()) {
+        myFile >> line;
+        if (line.substr(line.length() - 4, line.length()) == ".com") {
+            userProfile.insert(pair < string, list < string >> (line, userDetails));
+        }
+    }*/
+    cout << "Please enter The username :";
+    cin >> username;
+    verifyName();
+    if(is_username_existed(users, username)){
+        cout << "Sorry this username is already used Please choose another one\n\n";
+        Register();
+    }
+    else{
+        cout << "Please enter your email :";
+        cin >> email;
+        if(is_email_existed(users, email)){
+            cout << "Sorry this email is already used Please choose another one\n\n";
+            Register();
+        }
+        else{
+            verifyMobile();
+            verify_password();
+            users_file.open("users_file.txt",ios:: app);
+            users_file << username << endl;
+            users_file << email << endl;
+            users_file << encryptedPassword << endl;
+            users_file << mobile << endl;
+            users_file.close();
+        }
+    }
+}
+
+void load_existing_users(map<string, user_profile> &my_map){
+    users_file.open("users_file.txt", ios::in);
+    user_profile temp;
+    while(!users_file.eof()){
+        getline(users_file, temp.user_name);
+        getline(users_file, temp.user_email);
+        getline(users_file, temp.user_password);
+        getline(users_file, temp.user_mobile);
+        users.insert({temp.user_name, temp});
+    }
+    users_file.close();
+}
+
+
+bool is_username_existed(map <string, user_profile> &my_map, string username){
+    bool x = false;
+    for (auto const& element : my_map)
+    {
+        if(element.first == username){
+            x = true;
+            break;
+        }
+    }
+    return x;
+}
 void verifyName(){
-    string name;
-    cout << "Please enter your name :" << endl;
-    cin >> name;
     string namePattern = "^[a-zA-Z_]+$";
     regex nameRule(namePattern);
-    bool isValid = regex_search(name, nameRule);
+    bool isValid = regex_search(username, nameRule);
 
     if(!isValid){
         cout << "Invalid name, please enter letters or _ only: " << endl;
-        verifyName();
-    } else {
-        myFile.open("Database.txt", ios::app);
-        myFile << name << endl;
-        myFile.close();
-        userDetails.push_back(name);
+        Register();
     }
 }
 
 void verifyMobile(){
-    string mobile;
     cout << "Please enter your mobile number: " << endl;
     cin >> mobile;
-
     string regexPattern = "01[0-9]*";
     regex regexRule(regexPattern);
     bool isValid = regex_match(mobile, regexRule);
     if(mobile.length() != 11 || !isValid){
         cout << "Invalid mobile number, it must be 11 digits starts with 01: " << endl;
         verifyMobile();
-    } else {
-        myFile.open("Database.txt", ios::app);
-        myFile << mobile << endl;
-        myFile.close();
-        userDetails.push_back(mobile);
     }
 
 }
 
+
+
+bool is_email_existed(map <string, user_profile> &my_map, string email){
+    bool x = false;
+    for (auto const& element : my_map)
+    {
+        if(element.second.user_email == email){
+            x = true;
+            break;
+        }
+    }
+    return x;
+}
+bool is_password_existed(map <string, user_profile> &my_map){
+    string pass;
+    static int count = 0;
+    cout << "Please enter your password :";
+    cin >> pass;
+    bool x = false;
+    for (auto const& element : my_map)
+    {
+        if(element.second.user_password == pass){
+            x = true;
+            cout << "Successful login , Welcome to out login app!\n";
+            if(user_choice == "1"){
+                main();
+            }
+        }
+        else {
+            if (count == 2){
+                cout << "Your are denied access to the system \n\n";
+                main();
+            }
+            count++;
+            cout << "Sorry! Your password is not correct Please try again\n";
+            is_password_existed(users);
+        }
+
+
+    }
+    return x;
+}
 void verifyEmail(){
     string email;
     string regexPattern = "\\w+\\.?\\w+@\\w+\\.com";
@@ -70,17 +221,58 @@ void verifyEmail(){
     if(!isValid){
         cout << "Invalid email address format: [ohndoe@company.com] \nPlease try again! " << endl;
         verifyEmail();
-    } else {
-        userEmail = email;
-        for(auto word : userProfile){
-            if(email == word.first){
-                cout << "This email has been registered before, please choose another one: " << endl;
-                verifyEmail();
-                break;
-            }
-        }
     }
 }
+
+string verify_password(){
+    string passwordCheck;
+    cout << "Please make sure your password must:\n1. Contain upper and lowercase letters\n2. Contain symbols (ex: _, -, /)\n 3. Contain Numbers\n4. Contain no spaces\n5. Have at least 8 characters. ";
+    cout << "\nPassword: ";
+    cin >> password;
+    while (!has_any_digits(password)) {
+        cout << "Password must contain digits\nPlease re-enter your password: ";
+        cin >> password;
+        has_any_digits(password);
+    }
+    while (!check_uppercase(password) || !check_lowercase(password)) {
+        cout << "Password must contain both lower and uppercase letters\nPlease re-enter your password: ";
+        cin >> password;
+        check_lowercase(password);
+        check_uppercase(password);
+    }
+    while (password.length() < 8) {
+        cout << "Password must contain at least 8 characters\nPlease re-enter your password";
+        cin >> password;
+    }
+    while (!contains_symbols(password)) {
+        cout << "Password must contain symbols\nPlease re-enter your password: ";
+        cin >> password;
+        contains_symbols(password);
+    }
+    while (contains_spaces(password)) {
+        cout << "Password must not contain any spaces\nPlease re-enter your password: ";
+        cin >> password;
+        contains_spaces(password);
+    }
+    while (!contains_symbols(password)) {
+        cout <<  "Password must contain symbols\nPlease re-enter your password: ";
+        cin >> password;
+        contains_symbols(password);
+    }
+    cout << "Please re-enter your password to make sure it's correct: ";
+    cin >> passwordCheck;
+    while (password != passwordCheck) {
+        cout << "The password doesn't match the password you previously entered\nPlease re-enter your password: ";
+        cin >> passwordCheck;
+    }
+    encryptedPassword();
+    cout << "Your password has been safely saved\n";
+    return password;
+
+}
+
+
+
 
 void initializing_matrix() {
     for (int i = 0; i < key; i++) {
@@ -124,16 +316,14 @@ void encryption() {
     }
 }
 
-void encryptedPassword() {
-    string password;
-    cin.ignore();
-    getline(cin, password);
+string encryptedPassword() {
     auto removing_spaces = remove(password.begin(), password.end(), ' ');
     password.erase(removing_spaces, password.end());
     string_length = password.length();
     initializing_matrix();
     preparing_matrix_encryption (password);
     encryption();
+    return password;
 }
 
 bool has_any_digits(const string& password) {
@@ -155,127 +345,45 @@ bool contains_spaces(const string& password) {
     return password.find(' ') != -1;
 }
 
-string EnterPassword(){
-    string numAsString = "";
-    cin.ignore(256, '\n');
-    char ch = getch();
-    while (ch != '\n') {
-        if(ch != (char) 127){
-            cout << '*';
-            numAsString += ch;
-        }
-        else{
-            if(numAsString.length() != 0){
-                cout << "\b \b";
-                numAsString.resize(numAsString.length()-1);
-            }
-        }
-        ch = getch();
-    }
-    return numAsString;
-}
 
-void Register() {
-    string line;
-    myFile.open("Database.txt", ios::in);
-    while (!myFile.eof()) {
-        myFile >> line;
-        if (line.substr(line.length() - 4, line.length()) == ".com") {
-            userProfile.insert(pair < string, list < string >> (line, userDetails));
+void Login(){
+    string ID;
+    static int count = 0;
+    cout << "Please enter your username :";
+    cin >> ID;
+    if(is_username_existed(users, ID)){
+        is_password_existed(users);
+    }
+    else{
+        if (count == 2){
+            cout << "Your are denied access to the system \n\n";
+            main();
         }
+        count++;
+        cout << "Sorry! Your username is not correct Please try again\n";
+        Login();
     }
-    myFile.close();
-    // 1. add his profile  (with personal information) to the system and select a username and a password
-    // 2. user's email and email should not be previously registered
-    // 3. Ensure email follows proper email format
-    verifyEmail();
-    myFile.open("Database.txt", ios::app);
-    myFile << userEmail << endl;
-    myFile.close();
-    // 4. Verify mobile format
-    verifyMobile();
-    // 5. Ensure that the name follows proper format
-    verifyName();
-    char c;
-    string password, passwordCheck;
-    cout
-            << "Please make sure your password must:\n1. Contain upper and lowercase letters\n2. Contain symbols (ex: _, -, /)\n 3. Contain Numbers\n4. Contain no spaces\n5. Have at least 8 characters. ";
-    cout << "\nPassword: ";
-    password = EnterPassword();
-    while (!has_any_digits(password)) {
-        cout << "Password must contain digits\nPlease re-enter your password: ";
-        cin >> password;
-        has_any_digits(password);
-    }
-    while (!check_uppercase(password) || !check_lowercase(password)) {
-        cout << "Password must contain both lower and uppercase letters\nPlease re-enter your password: ";
-        cin >> password;
-        check_lowercase(password);
-        check_uppercase(password);
-    }
-    while (password.length() < 8) {
-        cout << "Password must contain at least 8 characters\nPlease re-enter your password";
-        cin >> password;
-    }
-    while (!contains_symbols(password)) {
-        cout << "Password must contain symbols\nPlease re-enter your password: ";
-        cin >> password;
-        contains_symbols(password);
-    }
-    while (contains_spaces(password)) {
-        cout << "Password must not contain any spaces\nPlease re-enter your password: ";
-        cin >> password;
-        contains_spaces(password);
-    }
-    while (!contains_symbols(password)) {
-        cout << "Password must contain symbols\nPlease re-enter your password: ";
-        cin >> password;
-        contains_symbols(password);
-    }
-    cout << "Please re-enter your password to make sure it's correct: ";
-    cin >> passwordCheck;
-    while (password != passwordCheck) {
-        cout << "The password doesn't match the password you previously entered\nPlease re-enter your password: ";
-        cin >> passwordCheck;
-    }
-    encryptedPassword();
-    cout << "You password has been safely saved";
-}
 
-
-void Login() {
 
 }
 
-void change_password() {
-
-}
-
-int main() {
-    while(true){
-        string user_choice;
-        cout << "What do you want to do ?\n";
-        cout << "1-Register\n";
-        cout << "2-Login\n";
-        cout << "3-Change Password\n";
-        cout << "4-Exit\n";
-        cin >> user_choice;
-        if (user_choice == "1"){
-            Register();
+void change_password(){
+    string word;
+    cout << "Please login at first\n";
+    Login();
+    cout << "Please enter your new password \n\n";
+    verify_password();
+    users_file.open("users_file.txt",ios:: in);
+    ofstream fileout("new_users_file.txt");
+    while(users_file >> word)
+    {
+        if(word == password){
+            word = "done";
+            //found = true;
         }
-        else if (user_choice  == "2"){
-            Login();
-        }
-        else if (user_choice == "3"){
-            change_password();
-        }
-        else if (user_choice == "4"){
-            break;
-        }
-        else{
-            cout << "Please enter a valid choice \n";
-        }
-
+        word += "\n";
+        fileout << word;
+        //if(found) break;
     }
-    return 0;
+
 }
